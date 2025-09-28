0703993e0c9a50f850017e8fd50c40436682b0bc
diego gtz contreras <legolas.black.ds@gmail.com>
2025-09-28 13:38:12 -0600

    Lista la integracion de sweetalert2

--- Front-app/register-ui/src/app/(login)/page.tsx
+++ Front-app/register-ui/src/app/(login)/page.tsx
@@ -1,13 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+//import Image from "next/image";
+//import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
-  return (
-    <>
-    <div className="row">
-      <div className="col-3">integracion de bootstrap</div>
-    <div className="col-3">calando clases de columnas</div>
-    </div>
-    </>
-  );
-}
+import Swal from 'sweetalert2'
+
+  export default function Home() {
+
+    const handleClick = () => {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esta acción no se puede deshacer.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, continuar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('¡Hecho!', 'La acción se completó correctamente.', 'success');
+      }
+    });
+  };
+    
+
+    return (
+      <>
+        <div className="row">
+          <div className="col-3">integracion de bootstrap</div>
+          <div className="col-3">calando clases de columnas</div>
+
+          <div className="container">
+            <h1 className="text-primary">Hola, Bootstrap con Next.js</h1>
+            <button className="btn btn-danger" onClick={handleClick}>
+              Click aquí
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+
+
+  
